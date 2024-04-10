286ae842e1844f871d3b0a67b07db5862c32a239
rd-120 <111455668+rd-120@users.noreply.github.com>
2024-04-10 22:44:22 +0300

    Update index.js

--- BackEnd/db/index.js
+++ BackEnd/db/index.js
@@ -7,7 +7,7 @@ const uri = process.env.URI;
 console.log();
 async function connect() {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect("mongodb+srv://desta:GKk0TIOTXPezpDOS@myapp.nhse7fb.mongodb.net/?retryWrites=true&w=majority&appName=MyApp");
     console.log('connected to MongoDB');
   } catch (error) {
     console.log('could not connect', error);
